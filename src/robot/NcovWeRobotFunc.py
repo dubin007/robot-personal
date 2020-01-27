2b50fa3d27e445d0140a7edee474e837b0035f78
Maicius <maicius@outlook.com>
2020-01-27 23:28:01 +0800

    取消关注的测试用例完成

--- src/robot/NcovWeRobotFunc.py
+++ src/robot/NcovWeRobotFunc.py
@@ -1,5 +1,6 @@
 import re
-from src.util.constant import ALL_AREA_KEY, AREA_TAIL, FIRST_NCOV_INFO, NO_NCOV_INFO, ORDER_KEY, UN_REGIST_PATTERN
+from src.util.constant import ALL_AREA_KEY, AREA_TAIL, FIRST_NCOV_INFO, NO_NCOV_INFO, ORDER_KEY, UN_REGIST_PATTERN, \
+    UN_REGIST_PATTERN2
 from src.util.log import LogSupport
 from src.util.redis_config import load_last_info
 
@@ -16,10 +17,12 @@ def user_subscribe(conn, user, area, jieba):
     :return:
     """
     all_area = set(conn.smembers(ALL_AREA_KEY))
+    if len(all_area) == 0:
+        ls.logging.error("all area key 为空")
     # 去掉订阅两字
     area = area.replace("订阅", '')
     area_list = jieba.cut(area)
-    area_list = list(filter(lambda x: len(x) == 1, area_list))
+    area_list = list(filter(lambda x: len(x) > 1, area_list))
     succ_subscribe = []
     failed_subscribe = []
     tails = ['省', '市', '区', '县','州','自治区', '自治州', '']
@@ -55,14 +58,16 @@ def user_unsubscribe_multi(conn, user, area, jieba):
     unsubscribe_list = []
     unsubscribe_list_fail = []
     # 全部取消订阅
-    if area.find("全部") != -1:
+    if area.find("全部") != -1 or area.find("全国") != -1:
         for area in all_order_area:
             conn.srem(area, user)
         unsubscribe_list.append("全部")
         return unsubscribe_list, unsubscribe_list_fail
-    area = re.subn(UN_REGIST_PATTERN, '', area)[0]
+    area = re.subn(UN_REGIST_PATTERN2, '', area)[0]
     area_list = jieba.cut(area)
     for ar in area_list:
+        flag = False
+        ar = re.subn(AREA_TAIL, '', ar)[0]
         if ar in all_order_area:
             ret = conn.srem(ar, user)
             if ret > 0:
@@ -73,12 +78,16 @@ def user_unsubscribe_multi(conn, user, area, jieba):
             # 比如用户订阅时使用的是湘西自治州，取消订阅时使用的湘西，则取消一个个查找
             for order_area in all_order_area:
                 if order_area.startswith(ar):
+                    flag = True
                     ret = conn.srem(order_area, user)
                     if ret > 0:
                         unsubscribe_list.append(order_area)
                     else:
                         unsubscribe_list_fail.append(order_area)
                     break
+            if not flag:
+                unsubscribe_list_fail.append(ar)
+
     return unsubscribe_list, unsubscribe_list_fail
 
 def get_ncvo_info_with_city(conn, citys):
--- test/testNcovWeRobot.py
+++ test/testNcovWeRobot.py
@@ -13,6 +13,40 @@ class testNcovWeRobot(unittest.TestCase):
 
     def setUp(self) -> None:
         self.sp = TXSpider()
+        self.data1 = [{"country": "中国", "area": "湖北", "city": "武汉", "confirm": 618, "suspect": 0, "dead": 45, "heal": 40},
+                 {"country": "中国", "area": "湖北", "city": "黄冈", "confirm": 122, "suspect": 0, "dead": 2, "heal": 2},
+                 {"country": "中国", "area": "湖北", "city": "孝感", "confirm": 55, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "荆门", "confirm": 38, "suspect": 0, "dead": 1, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "恩施州", "confirm": 17, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "荆州", "confirm": 33, "suspect": 0, "dead": 2, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "仙桃", "confirm": 11, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "十堰", "confirm": 20, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "随州", "confirm": 36, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "天门", "confirm": 5, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "宜昌", "confirm": 20, "suspect": 0, "dead": 1, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "鄂州", "confirm": 1, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "咸宁", "confirm": 43, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "广东", "city": "广州", "confirm": 17, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "加拿大", "area": "", "city": "", "confirm": 1, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "法国", "area": "", "city": "", "confirm": 3, "suspect": 0, "dead": 0, "heal": 0}]
+
+        self.data2 = [{"country": "中国", "area": "湖北", "city": "武汉", "confirm": 698, "suspect": 0, "dead": 45, "heal": 40},
+                 {"country": "中国", "area": "湖北", "city": "黄冈", "confirm": 122, "suspect": 0, "dead": 2, "heal": 2},
+                 {"country": "中国", "area": "湖北", "city": "孝感", "confirm": 55, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "荆门", "confirm": 38, "suspect": 0, "dead": 1, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "恩施州", "confirm": 17, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "荆州", "confirm": 33, "suspect": 0, "dead": 2, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "仙桃", "confirm": 11, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "十堰", "confirm": 20, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "随州", "confirm": 36, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "天门", "confirm": 5, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "宜昌", "confirm": 20, "suspect": 0, "dead": 1, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "鄂州", "confirm": 1, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "湖北", "city": "咸宁", "confirm": 43, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "中国", "area": "广东", "city": "广州", "confirm": 17, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "加拿大", "area": "", "city": "", "confirm": 2, "suspect": 0, "dead": 0, "heal": 0},
+                 {"country": "法国", "area": "", "city": "", "confirm": 3, "suspect": 0, "dead": 0, "heal": 0},
+                {"country": "中国", "area": "重庆", "city": "重庆", "confirm": 3, "suspect": 0, "dead": 0, "heal": 0}]
 
     def testCheckRegister(self):
         assert check_whether_register("订阅湖北") == True
@@ -32,43 +66,26 @@ class testNcovWeRobot(unittest.TestCase):
         succ, failed = user_subscribe(conn, 'test', '订阅湖北', jieba)
         assert succ == ['湖北']
 
-    def test_do_ncov_update(self):
-        data1 = [{"country": "中国", "area": "湖北", "city": "武汉", "confirm": 618, "suspect": 0, "dead": 45, "heal": 40},
-                {"country": "中国", "area": "湖北", "city": "黄冈", "confirm": 122, "suspect": 0, "dead": 2, "heal": 2},
-                {"country": "中国", "area": "湖北", "city": "孝感", "confirm": 55, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "荆门", "confirm": 38, "suspect": 0, "dead": 1, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "恩施州", "confirm": 17, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "荆州", "confirm": 33, "suspect": 0, "dead": 2, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "仙桃", "confirm": 11, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "十堰", "confirm": 20, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "随州", "confirm": 36, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "天门", "confirm": 5, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "宜昌", "confirm": 20, "suspect": 0, "dead": 1, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "鄂州", "confirm": 1, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "咸宁", "confirm": 43, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "广东", "city": "广州", "confirm": 17, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "加拿大", "area": "", "city": "", "confirm": 1, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "法国", "area": "", "city": "", "confirm": 3, "suspect": 0, "dead": 0, "heal": 0}]
-
-        data2 = [{"country": "中国", "area": "湖北", "city": "武汉", "confirm": 698, "suspect": 0, "dead": 45, "heal": 40},
-                {"country": "中国", "area": "湖北", "city": "黄冈", "confirm": 122, "suspect": 0, "dead": 2, "heal": 2},
-                {"country": "中国", "area": "湖北", "city": "孝感", "confirm": 55, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "荆门", "confirm": 38, "suspect": 0, "dead": 1, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "恩施州", "confirm": 17, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "荆州", "confirm": 33, "suspect": 0, "dead": 2, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "仙桃", "confirm": 11, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "十堰", "confirm": 20, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "随州", "confirm": 36, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "天门", "confirm": 5, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "宜昌", "confirm": 20, "suspect": 0, "dead": 1, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "鄂州", "confirm": 1, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "湖北", "city": "咸宁", "confirm": 43, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "中国", "area": "广东", "city": "广州", "confirm": 17, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "加拿大", "area": "", "city": "", "confirm": 2, "suspect": 0, "dead": 0, "heal": 0},
-                {"country": "法国", "area": "", "city": "", "confirm": 3, "suspect": 0, "dead": 0, "heal": 0}]
+    def test_user_unsubscribe(self):
+        conn = connect_redis()
         # 完成数据转化并更新数据库
-        last = self.sp.change_raw_data_format(data1)
-        now = self.sp.change_raw_data_format(data2)
+        self.sp.change_raw_data_format(self.data2)
+        user_subscribe(conn, 'test', '订阅湖北', jieba)
+        succ, failed = user_unsubscribe_multi(conn, 'test', '取消关注湖北', jieba)
+        assert succ == ['湖北']
+        succ, failed = user_subscribe(conn, 'test', '订阅湖北重庆', jieba)
+        assert succ == ['湖北', '重庆']
+        succ, failed = user_unsubscribe_multi(conn, 'test', '取消重庆市', jieba)
+        assert succ == ['重庆']
+        succ, failed = user_unsubscribe_multi(conn, 'test', '取关全国', jieba)
+        assert succ == ['全部']
+        succ, failed = user_unsubscribe_multi(conn, 'test', '取消湖南', jieba)
+        assert succ == [] and failed == ['湖南']
+
+    def test_do_ncov_update(self):
+        # 完成数据转化并更新数据库
+        last = self.sp.change_raw_data_format(self.data1)
+        now = self.sp.change_raw_data_format(self.data2)
         update_city = self.sp.parse_increase_info(now, last)
         self.sp.re.set(SHOULD_UPDATE, 1)
         save_json_info_as_key(self.sp.re, UPDATE_CITY, update_city)
